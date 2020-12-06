d2f0eb2ac244de2d66b67eb3a4be44b5937df7f3
Tigermouthbear <tigermouthbear@tigr.dev>
2020-12-05 21:34:04 -0500

    add automatic beta builds

---
+++ scripts/travis_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# builds artifacts if on main branch, else just check
+
+if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+  ./gradlew check
+else
+  ./gradlew buildAll
+fi
---
+++ scripts/travis_upload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# uploads built artifacts to aresclient.org if branch is master and not a pr
+
+if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+  exit 0
+fi
+
+for filename in $TRAVIS_BUILD_DIR/build/*.jar; do
+    res=$(curl -F token=$UPLOAD_TOKEN -F file=@$TRAVIS_BUILD_DIR/build/$filename https://aresclient.org/beta>
+    echo $res
+done
