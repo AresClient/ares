28784dac6a9b2963598feb0b85d1849063a91d05
Tigermouthbear <tigermouthbear@tigr.dev>
2020-12-07 17:14:16 -0500

    added beta commit message uploading

--- scripts/travis_upload.sh
+++ scripts/travis_upload.sh
@@ -6,6 +6,6 @@ if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
 fi
 
 for filename in $TRAVIS_BUILD_DIR/build/*.jar; do
-    res=$(curl -F token=$UPLOAD_TOKEN -F file=@$filename https://aresclient.org/beta)
+    res=$(curl -F token=$UPLOAD_TOKEN -F message=$TRAVIS_COMMIT_MESSAGE -F file=@$filename https://aresclient.org/beta)
     echo $res
 done
