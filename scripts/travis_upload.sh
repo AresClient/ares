87021831d05c6f348c7e70a8867b8aced45c651d
Tigermouthbear <tigermouthbear@tigr.dev>
2020-12-07 22:53:24 -0500

    update upload script

--- scripts/travis_upload.sh
+++ scripts/travis_upload.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-# uploads built artifacts to aresclient.org if branch is master and not a pr
+# uploads build artifacts to aresclient.org if branch is master and not a pr
 
 if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
   exit 0
 fi
 
+FORGE_PATH = ""
+FABRIC_PATH = ""
+
 for filename in $TRAVIS_BUILD_DIR/build/*.jar; do
-    res=$(curl -F token=$UPLOAD_TOKEN -F message=$TRAVIS_COMMIT_MESSAGE -F file=@$filename https://aresclient.org/beta)
-    echo $res
+  if [ filename == *"forge" ]; then
+    FORGE_PATH = filename
+  fi
+  if [ filename == *"fabric" ]; then
+    FABRIC_PATH = filename
+  fi
 done
+
+if [ -z "$FORGE_PATH" ] && [ -z "$FABRIC_PATH" ]; then
+  echo $(curl -F token=$UPLOAD_TOKEN -F message="$TRAVIS_COMMIT_MESSAGE" -F forge=@$FORGE_PATH -F fabric=@$FABRIC_PATH https://aresclient.org/beta)
+else
+  echo "Couldn't find path to forge and fabric builds!"
+fi
