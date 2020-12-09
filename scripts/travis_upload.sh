c907573b05c8c8cc70f5166be33acb00a762f483
Tigermouthbear <tigermouthbear@tigr.dev>
2020-12-08 21:09:30 -0500

    fix beta upload build finding

--- scripts/travis_upload.sh
+++ scripts/travis_upload.sh
@@ -9,15 +9,15 @@ FORGE_PATH=""
 FABRIC_PATH=""
 
 for filename in $TRAVIS_BUILD_DIR/build/*.jar; do
-  if [ filename == *"forge" ]; then
-    FORGE_PATH=$filename
+  if grep -q "forge" <<< "$filename"; then
+    FORGE_PATH="$filename"
   fi
-  if [ filename == *"fabric" ]; then
-    FABRIC_PATH=$filename
+  if grep -q "fabric" <<< "$filename"; then
+    FABRIC_PATH="$filename"
   fi
 done
 
-if [ -z "$FORGE_PATH" ] && [ -z "$FABRIC_PATH" ]; then
+if [ $FORGE_PATH != "" ] && [ $FABRIC_PATH  != "" ]; then
   echo $(curl -F token=$UPLOAD_TOKEN -F message="$TRAVIS_COMMIT_MESSAGE" -F forge=@$FORGE_PATH -F fabric=@$FABRIC_PATH https://aresclient.org/beta)
 else
   echo "Couldn't find path to forge and fabric builds!"
