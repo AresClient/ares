b105803090aee1d8fdcb33469e9220449f2268a8
Tigermouthbear <tigermouthbear@tigr.dev>
2021-07-22 18:05:58 -0400

    fix autosprint not working in all directions, closes #34

--- ares-fabric-1.16/src/main/java/dev/tigr/ares/fabric/impl/modules/movement/AutoSprint.java
+++ ares-fabric-1.16/src/main/java/dev/tigr/ares/fabric/impl/modules/movement/AutoSprint.java
@@ -3,14 +3,34 @@ package dev.tigr.ares.fabric.impl.modules.movement;
 import dev.tigr.ares.core.feature.module.Category;
 import dev.tigr.ares.core.feature.module.Module;
 
-
 /**
  * @author Tigermouthbear
  */
 @Module.Info(name = "AutoSprint", description = "Makes player always sprinting in any direction", category = Category.MOVEMENT)
 public class AutoSprint extends Module {
+    private static final float SPRINT_SPEED = 0.28061673f;
+
     @Override
     public void onMotion() {
-        MC.player.setSprinting(true);
+        if(MC.player.input.movementForward != 0 || MC.player.input.movementSideways != 0) {
+            MC.player.setSprinting(true);
+
+            if(!MC.player.isOnGround()) return;
+
+            float yaw = MC.player.yaw;
+            float forward = 1;
+
+            if(MC.player.forwardSpeed < 0) {
+                yaw += 180;
+                forward = -0.5f;
+            } else if(MC.player.forwardSpeed > 0) forward = 0.5f;
+
+            if(MC.player.sidewaysSpeed > 0) yaw -= 90 * forward;
+            if(MC.player.sidewaysSpeed < 0) yaw += 90 * forward;
+
+            yaw = (float) Math.toRadians(yaw);
+
+            MC.player.setVelocity(-Math.sin(yaw) * SPRINT_SPEED, MC.player.getVelocity().y, Math.cos(yaw) * SPRINT_SPEED);
+        }
     }
 }
--- ares-forge/src/main/java/dev/tigr/ares/forge/impl/modules/movement/AutoSprint.java
+++ ares-forge/src/main/java/dev/tigr/ares/forge/impl/modules/movement/AutoSprint.java
@@ -8,8 +8,30 @@ import dev.tigr.ares.core.feature.module.Module;
  */
 @Module.Info(name = "AutoSprint", description = "Makes player always sprinting in any direction", category = Category.MOVEMENT)
 public class AutoSprint extends Module {
+    private static final float SPRINT_SPEED = 0.28061673f;
+
     @Override
     public void onMotion() {
-        MC.player.setSprinting(true);
+        if(MC.player.movementInput.moveForward != 0 || MC.player.movementInput.moveStrafe != 0) {
+            MC.player.setSprinting(true);
+
+            if(!MC.player.onGround) return;
+
+            float yaw = MC.player.rotationYaw;
+            float forward = 1;
+
+            if(MC.player.moveForward < 0) {
+                yaw += 180;
+                forward = -0.5f;
+            } else if(MC.player.moveForward > 0) forward = 0.5f;
+
+            if(MC.player.moveStrafing > 0) yaw -= 90 * forward;
+            if(MC.player.moveStrafing < 0) yaw += 90 * forward;
+
+            yaw = (float) Math.toRadians(yaw);
+
+            MC.player.motionX = -Math.sin(yaw) * SPRINT_SPEED;
+            MC.player.motionZ = Math.cos(yaw) * SPRINT_SPEED;
+        }
     }
 }
