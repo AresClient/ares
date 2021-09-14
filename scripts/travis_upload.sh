2dd031e87a7c7699b6db82e32f24b4f591168dba
Tigermouthbear <tigermouthbear@tigr.dev>
2021-09-14 11:05:47 -0400

    update CI for 1.16 fabric version

--- scripts/travis_upload.sh
+++ scripts/travis_upload.sh
@@ -7,6 +7,7 @@ fi
 
 FORGE_PATH=""
 FABRIC_PATH=""
+FABRIC_16_PATH=""
 
 for filename in $TRAVIS_BUILD_DIR/build/*.jar; do
   if grep -q "forge" <<< "$filename"; then
@@ -15,11 +16,14 @@ for filename in $TRAVIS_BUILD_DIR/build/*.jar; do
   if grep -q "fabric" <<< "$filename" && grep -qv "1.16" <<< "$filename"; then
     FABRIC_PATH="$filename"
   fi
+  if grep -q "fabric" <<< "$filename" && grep -q "1.16" <<< "$filename"; then
+    FABRIC_16_PATH="$filename"
+  fi
 done
 
 COMMITS=$(git log $TRAVIS_COMMIT_RANGE --oneline | tac)
 if [ $FORGE_PATH != "" ] && [ $FABRIC_PATH  != "" ]; then
-  echo $(curl -F token=$UPLOAD_TOKEN -F message="$COMMITS" -F forge=@$FORGE_PATH -F fabric=@$FABRIC_PATH https://aresclient.org/beta)
+  echo $(curl -F token=$UPLOAD_TOKEN -F message="$COMMITS" -F forge=@$FORGE_PATH -F fabric=@$FABRIC_PATH fabric_16=@$FABRIC_16_PATH https://aresclient.org/beta)
 else
   echo "Couldn't find path to forge and fabric builds!"
 fi
