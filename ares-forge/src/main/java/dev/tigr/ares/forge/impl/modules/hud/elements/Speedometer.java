0bcd4167a5c74381bdaf8ea5f921c58f39ed007a
Doogie13 <jay.finbow13@gmail.com>
2021-10-01 16:01:30 +0100

    Speedo fixed, added motion mode

--- ares-forge/src/main/java/dev/tigr/ares/forge/impl/modules/hud/elements/Speedometer.java
+++ ares-forge/src/main/java/dev/tigr/ares/forge/impl/modules/hud/elements/Speedometer.java
@@ -5,6 +5,7 @@ import dev.tigr.ares.core.feature.module.Module;
 import dev.tigr.ares.core.setting.Setting;
 import dev.tigr.ares.core.setting.settings.BooleanSetting;
 import dev.tigr.ares.core.setting.settings.EnumSetting;
+import dev.tigr.ares.core.setting.settings.numerical.IntegerSetting;
 import dev.tigr.ares.core.util.global.ReflectionHelper;
 import dev.tigr.ares.core.util.global.Utils;
 import dev.tigr.ares.core.util.render.Color;
@@ -13,21 +14,26 @@ import dev.tigr.ares.forge.impl.modules.hud.HudElement;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.Timer;
 
+import java.util.Set;
+
 /**
  * @author Tigermouthbear
  */
 @Module.Info(name = "Speedometer", description = "Shows your speed", category = Category.HUD)
 public class Speedometer extends HudElement {
+
     private final Setting<Boolean> rainbow = register(new BooleanSetting("Rainbow", false));
     private final Setting<SpeedUnits> speedUnit = register(new EnumSetting<>("Unit", SpeedUnits.KILOMETERS_PH));
     
     enum SpeedUnits {
         METERS_PS,
         MILES_PH,
-        KILOMETERS_PH
+        KILOMETERS_PH,
+        MOTION
     }
 
     private double speed = 0;
+    private double speedc = 0;
 
     public Speedometer() {
         super(150, 60, 0, FONT_RENDERER.getFontHeight());
@@ -35,7 +41,6 @@ public class Speedometer extends HudElement {
 
     @Override
     public void onTick() {
-        if(MC.player.ticksExisted % 10 != 0) return;
 
         float tickLength = ReflectionHelper.getPrivateValue(Timer.class, ReflectionHelper.getPrivateValue(Minecraft.class, MC, "timer", "field_71428_T"), "tickLength", "field_194149_e");
         double tps = tickLength / 1000;
@@ -43,6 +48,13 @@ public class Speedometer extends HudElement {
         double xMove = MC.player.posX - MC.player.prevPosX;
         double zMove = MC.player.posZ - MC.player.prevPosZ;
         speed = Math.sqrt(xMove * xMove + zMove * zMove) / tps;
+
+        if (!(MC.player.ticksExisted % 2 == 0))
+            speedc = (speed + speedc) / 2;
+        else {
+            speed = speedc;
+            speedc = speed;
+        }
     }
 
     public void draw() {
@@ -54,8 +66,11 @@ public class Speedometer extends HudElement {
             case MILES_PH:
                 str = Utils.roundDouble(speed * 2.23694, 1) + " MPH";
                 break;
-            default:
+            case KILOMETERS_PH:
                 str = Utils.roundDouble(speed * 3.6, 1) + " KM/H";
+                break;
+            default: // guarantee initialisation of "str"
+                str = Utils.roundDouble(speed / 20, 1) + " BPT";
         }
 
         drawString(str, getX(), getY(), rainbow.getValue() ? IRenderer.rainbow() : Color.WHITE);
