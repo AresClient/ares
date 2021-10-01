e37a2a7bdaf91d89da74f31d5606e9db5ac8bf95
Doogie13 <jay.finbow13@gmail.com>
2021-10-01 16:02:10 +0100

    Timer instance, velocity fixed

--- ares-forge/src/main/java/dev/tigr/ares/forge/impl/modules/movement/Timer.java
+++ ares-forge/src/main/java/dev/tigr/ares/forge/impl/modules/movement/Timer.java
@@ -3,24 +3,40 @@ package dev.tigr.ares.forge.impl.modules.movement;
 import dev.tigr.ares.core.feature.module.Category;
 import dev.tigr.ares.core.feature.module.Module;
 import dev.tigr.ares.core.setting.Setting;
+import dev.tigr.ares.core.setting.settings.EnumSetting;
+import dev.tigr.ares.core.setting.settings.numerical.FloatSetting;
 import dev.tigr.ares.core.setting.settings.numerical.IntegerSetting;
 import dev.tigr.ares.core.util.global.ReflectionHelper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.math.Vec2f;
 
 /**
  * @author Tigermouthbear
  */
 @Module.Info(name = "Timer", description = "Sets client side tps", category = Category.MOVEMENT)
 public class Timer extends Module {
-    private final Setting<Integer> tps = register(new IntegerSetting("TPS", 25, 1, 160));
+    private final Setting<Float> tps = register(new FloatSetting("Speed", 1.088f, 0.1f, 20f));
+    private final Setting<when> mode = register(new EnumSetting<when>("Mode", when.ALWAYS));
+
+    public static Timer INSTANCE;
+
+    public Timer() {
+        INSTANCE = this;
+    }
 
     @Override
     public void onTick() {
-        ReflectionHelper.setPrivateValue(net.minecraft.util.Timer.class, ReflectionHelper.getPrivateValue(Minecraft.class, MC, "timer", "field_71428_T"), 1000.0F / tps.getValue(), "tickLength", "field_194149_e");
+        if (!(MC.player.moveForward == 0) && mode.getValue() == when.MOTION
+        || mode.getValue() == when.ALWAYS){
+            ReflectionHelper.setPrivateValue(net.minecraft.util.Timer.class, ReflectionHelper.getPrivateValue(Minecraft.class, MC, "timer", "field_71428_T"), (1000.0F / tps.getValue()) / 20, "tickLength", "field_194149_e");
+        }
     }
 
     @Override
     public void onDisable() {
         ReflectionHelper.setPrivateValue(net.minecraft.util.Timer.class, ReflectionHelper.getPrivateValue(Minecraft.class, MC, "timer", "field_71428_T"), 1000.0F / 20.0F, "tickLength", "field_194149_e");
     }
+
+    enum when {ALWAYS, MOTION}
+
 }
