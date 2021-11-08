b914a25235ba33248471151d3590e0d65d58558a
m <makrommel@protonmail.ch>
2021-11-08 04:36:35 +0000

    Added render options to BurrowDetect

--- ares-fabric/src/main/java/dev/tigr/ares/fabric/impl/modules/combat/BurrowDetect.java
+++ ares-fabric/src/main/java/dev/tigr/ares/fabric/impl/modules/combat/BurrowDetect.java
@@ -3,13 +3,18 @@ package dev.tigr.ares.fabric.impl.modules.combat;
 import dev.tigr.ares.core.feature.module.Category;
 import dev.tigr.ares.core.feature.module.Module;
 import dev.tigr.ares.core.setting.Setting;
+import dev.tigr.ares.core.setting.settings.BooleanSetting;
 import dev.tigr.ares.core.setting.settings.numerical.DoubleSetting;
+import dev.tigr.ares.core.setting.settings.numerical.FloatSetting;
+import dev.tigr.ares.core.util.render.Color;
 import dev.tigr.ares.core.util.render.TextColor;
 import dev.tigr.ares.fabric.utils.WorldUtils;
 import dev.tigr.ares.fabric.utils.entity.SelfUtils;
+import dev.tigr.ares.fabric.utils.render.RenderUtils;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 
 import java.util.ArrayList;
@@ -22,6 +27,13 @@ import java.util.List;
 @Module.Info(name = "BurrowDetect", description = "Sends a message in chat whenever a player is burrowed", category = Category.COMBAT)
 public class BurrowDetect extends Module {
     private final Setting<Double> radius = register(new DoubleSetting("Radius", 20, 0, 100));
+    private final Setting<Boolean> notification = register(new BooleanSetting("Notification", true));
+    private final Setting<Boolean> render = register(new BooleanSetting("Render", true));
+    private final Setting<Float> colorRed = register(new FloatSetting("Red", 0.6f, 0, 1)).setVisibility(render::getValue);
+    private final Setting<Float> colorGreen = register(new FloatSetting("Green", 0, 0, 1)).setVisibility(render::getValue);
+    private final Setting<Float> colorBlue = register(new FloatSetting("Blue", 0.6f, 0, 1)).setVisibility(render::getValue);
+    private final Setting<Float> fillAlpha = register(new FloatSetting("Fill Alpha", 0.24f, 0, 1)).setVisibility(render::getValue);
+    private final Setting<Float> boxAlpha = register(new FloatSetting("Box Alpha", 1f, 0, 1)).setVisibility(render::getValue);
 
     private final List<PlayerEntity> burrowedPlayers = new ArrayList<>();
 
@@ -29,7 +41,7 @@ public class BurrowDetect extends Module {
     public void onTick() {
         SelfUtils.getPlayersInRadius(radius.getValue()).stream().filter(entityPlayer -> entityPlayer != MC.player).forEach(playerEntity -> {
             if(!burrowedPlayers.contains(playerEntity) && isInBurrow(playerEntity)) {
-                UTILS.printMessage(TextColor.BLUE + playerEntity.getEntityName() + TextColor.GREEN + " has burrowed");
+                if(notification.getValue()) UTILS.printMessage(TextColor.BLUE + playerEntity.getEntityName() + TextColor.GREEN + " has burrowed");
                 burrowedPlayers.add(playerEntity);
             }
         });
@@ -37,11 +49,33 @@ public class BurrowDetect extends Module {
         try {
             for(PlayerEntity playerEntity : burrowedPlayers) {
                 if(!isInBurrow(playerEntity)) {
-                    UTILS.printMessage(TextColor.BLUE + playerEntity.getEntityName() + TextColor.RED + " is no longer burrowed");
+                    if(notification.getValue()) UTILS.printMessage(TextColor.BLUE + playerEntity.getEntityName() + TextColor.RED + " is no longer burrowed");
                     burrowedPlayers.remove(playerEntity);
                 }
             }
         } catch(ConcurrentModificationException ignore) {}
+    }
+
+    @Override
+    public void onRender3d() {
+        if(!render.getValue() || burrowedPlayers.isEmpty()) return;
+
+        Color fillColor = new Color(colorRed.getValue(), colorGreen.getValue(), colorBlue.getValue(), fillAlpha.getValue());
+        Color outlineColor = new Color(colorRed.getValue(), colorGreen.getValue(), colorBlue.getValue(), boxAlpha.getValue());
+
+        RenderUtils.prepare3d();
+
+        for(PlayerEntity player: burrowedPlayers) {
+            if(!isInBurrow(player)) continue;
+
+            BlockPos pos = new BlockPos(getMiddlePosition(player.getX()), player.getY(), getMiddlePosition(player.getZ()));
+            BlockPos playerPos = WorldUtils.roundBlockPos(new Vec3d(pos.getX(), player.getY(), pos.getZ()));
+
+            Box box = RenderUtils.getBoundingBox(playerPos);
+            RenderUtils.cube(box, fillColor, outlineColor);
+        }
+
+        RenderUtils.end3d();
     }
 
     private boolean isInBurrow(PlayerEntity playerEntity) {
--- ares-forge/src/main/java/dev/tigr/ares/forge/impl/modules/combat/BurrowDetect.java
+++ ares-forge/src/main/java/dev/tigr/ares/forge/impl/modules/combat/BurrowDetect.java
@@ -3,12 +3,17 @@ package dev.tigr.ares.forge.impl.modules.combat;
 import dev.tigr.ares.core.feature.module.Category;
 import dev.tigr.ares.core.feature.module.Module;
 import dev.tigr.ares.core.setting.Setting;
+import dev.tigr.ares.core.setting.settings.BooleanSetting;
 import dev.tigr.ares.core.setting.settings.numerical.DoubleSetting;
+import dev.tigr.ares.core.setting.settings.numerical.FloatSetting;
+import dev.tigr.ares.core.util.render.Color;
 import dev.tigr.ares.core.util.render.TextColor;
 import dev.tigr.ares.forge.utils.WorldUtils;
 import dev.tigr.ares.forge.utils.entity.SelfUtils;
+import dev.tigr.ares.forge.utils.render.RenderUtils;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
@@ -24,6 +29,13 @@ import java.util.stream.Collectors;
 @Module.Info(name = "BurrowDetect", description = "Sends a message in chat whenever a player is burrowed", category = Category.COMBAT)
 public class BurrowDetect extends Module {
     private final Setting<Double> radius = register(new DoubleSetting("Radius", 20, 0, 100));
+    private final Setting<Boolean> notification = register(new BooleanSetting("Notification", true));
+    private final Setting<Boolean> render = register(new BooleanSetting("Render", true));
+    private final Setting<Float> colorRed = register(new FloatSetting("Red", 0.6f, 0, 1)).setVisibility(render::getValue);
+    private final Setting<Float> colorGreen = register(new FloatSetting("Green", 0, 0, 1)).setVisibility(render::getValue);
+    private final Setting<Float> colorBlue = register(new FloatSetting("Blue", 0.6f, 0, 1)).setVisibility(render::getValue);
+    private final Setting<Float> fillAlpha = register(new FloatSetting("Fill Alpha", 0.24f, 0, 1)).setVisibility(render::getValue);
+    private final Setting<Float> boxAlpha = register(new FloatSetting("Box Alpha", 1f, 0, 1)).setVisibility(render::getValue);
 
     private final List<EntityPlayer> burrowedPlayers = new ArrayList<>();
 
@@ -31,7 +43,7 @@ public class BurrowDetect extends Module {
     public void onTick(){
         for(EntityPlayer entityPlayer: SelfUtils.getPlayersInRadius(radius.getValue()).stream().filter(entityPlayer -> entityPlayer != MC.player).collect(Collectors.toList())){
             if(!burrowedPlayers.contains(entityPlayer) && isInBurrow(entityPlayer)){
-                UTILS.printMessage(TextColor.BLUE + entityPlayer.getDisplayNameString() + TextColor.GREEN + " has burrowed");
+                if(notification.getValue()) UTILS.printMessage(TextColor.BLUE + entityPlayer.getDisplayNameString() + TextColor.GREEN + " has burrowed");
                 burrowedPlayers.add(entityPlayer);
             }
         }
@@ -39,11 +51,33 @@ public class BurrowDetect extends Module {
         try {
             for(EntityPlayer entityPlayer : burrowedPlayers){
                 if(!isInBurrow(entityPlayer)){
-                    UTILS.printMessage(TextColor.BLUE + entityPlayer.getDisplayNameString() + TextColor.RED + " is no longer burrowed");
+                    if(notification.getValue()) UTILS.printMessage(TextColor.BLUE + entityPlayer.getDisplayNameString() + TextColor.RED + " is no longer burrowed");
                     burrowedPlayers.remove(entityPlayer);
                 }
             }
         } catch(ConcurrentModificationException ignore) {}
+    }
+
+    @Override
+    public void onRender3d() {
+        if(!render.getValue() || burrowedPlayers.isEmpty()) return;
+
+        Color fillColor = new Color(colorRed.getValue(), colorGreen.getValue(), colorBlue.getValue(), fillAlpha.getValue());
+        Color outlineColor = new Color(colorRed.getValue(), colorGreen.getValue(), colorBlue.getValue(), boxAlpha.getValue());
+
+        RenderUtils.prepare3d();
+
+        for(EntityPlayer player: burrowedPlayers) {
+            if(!isInBurrow(player)) continue;
+
+            BlockPos pos = new BlockPos(getMiddlePosition(player.posX), player.posY, getMiddlePosition(player.posZ));
+            BlockPos playerPos = WorldUtils.roundBlockPos(new Vec3d(pos.getX(), player.posY, pos.getZ()));
+
+            AxisAlignedBB box = RenderUtils.getBoundingBox(playerPos);
+            RenderUtils.cube(box, fillColor, outlineColor);
+        }
+
+        RenderUtils.end3d();
     }
 
     private boolean isInBurrow(EntityPlayer entityPlayer){
