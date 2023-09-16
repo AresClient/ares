35e8a16e6cc8097ccdcd9d6b7c17bd006abf8be3
Makrennel <makrommel@protonmail.ch>
2023-09-16 03:46:58 +0100

    Switch Interaction from Rate to Delay

--- core/src/main/kotlin/org/aresclient/ares/impl/instrument/global/Interaction.kt
+++ core/src/main/kotlin/org/aresclient/ares/impl/instrument/global/Interaction.kt
@@ -1,31 +1,28 @@
 package org.aresclient.ares.impl.instrument.global
 
 import org.aresclient.ares.api.instrument.global.Global
-import kotlin.math.round
+import org.aresclient.ares.api.setting.Setting
+import kotlin.math.ceil
 
 interface Placer: Prioritizer {
-    fun placeRate() = Interaction.placeRate(Interaction.placeRate.value)
-    fun placeDelay() = 1000 - placeRate()
-    fun placeDelayTick() = 20 - placeDelay() / 50
-    fun placePerInstant() = Interaction.placePerInstant.value
+    fun placeDelay() = Interaction.delaySettingAsMillis(Interaction.block_place_delay)
+    fun placeDelayTick() = Interaction.millisToTick(placeDelay())
+    fun placePerInstant() = Interaction.place_per_instant.value
 }
 
 interface Breaker: Prioritizer {
-    fun breakRate() = Interaction.breakRate(Interaction.breakRate.value)
-    fun breakDelay() = 1000 - breakRate()
-    fun breakDelayTick() = 20 - breakRate() / 50
+    fun breakDelay() = Interaction.delaySettingAsMillis(Interaction.block_break_delay)
+    fun breakDelayTick() = Interaction.millisToTick(breakDelay())
 }
 
 interface Crystaller: Prioritizer {
-    fun crystalRate() = Interaction.crystalRate(Interaction.crystalRate.value)
-    fun crystalDelay() = 1000 - crystalRate()
-    fun crystalDelayTick() = 20 - crystalRate() / 50
+    fun crystalPlaceDelay() = Interaction.delaySettingAsMillis(Interaction.crystal_place_delay)
+    fun crystalPlaceDelayTick() = Interaction.millisToTick(crystalPlaceDelay())
 }
 
 interface Attacker: Prioritizer {
-    fun attackRate() = Interaction.attackRate(Interaction.attackRate.value)
-    fun attackDelay() = 1000 - attackRate()
-    fun attackDelayTick() = 20 - attackRate() / 50
+    fun attackDelay() = Interaction.delaySettingAsMillis(Interaction.attack_delay)
+    fun attackDelayTick() = Interaction.millisToTick(attackDelay())
 }
 
 object Interaction: Global("Interaction", "Handles player interactions with blocks and entities") {
@@ -34,21 +31,38 @@ object Interaction: Global("Interaction", "Handles player interactions with bloc
         MILLISECOND // TODO: Separate Multithreaded Millisecond tick
     }
 
-    val rateType = settings.addEnum("Rate Type", RateType.TICK,
-        "The way delays are timed",
-        "Tick - Minecraft's client tick which runs once every twentieth of a second (1/20)",
-        "Millisecond - Use a separate thread which runs once every thousandth of a second (1/1000)"
-    )
+    val percentageMeasure = "Measure: Percentage of a second"
+    val percentageMeasureTick = "If Global/Interaction/Delay_Type is set to Tick this only functions in units of 5"
+    val percentageMeasureArray = arrayOf(percentageMeasure, percentageMeasureTick)
+    val tickMeasure = "Measure: Tick (One twentieth of a second)"
+    val instantDefine = "One instant is one tick or millisecond, depending on whatever Global/Interaction/Delay_Type is set to"
+
+    val delay_type = settings.addEnum("Delay Type", RateType.TICK)
+        .appendLines("The way delays are timed",)
+        .appendLines("Tick - Minecraft's client tick which runs once every twentieth of a second (1/20)")
+        .appendLines("Millisecond - Use a separate thread which runs once every thousandth of a second (1/1000)")
 
     // Use as a percentage with one decimal point so that it makes sense as both a tick and millisecond setting
-    val placeRate = settings.addDouble("Block Place Rate %", 100.0).setMin(0.0).setMax(100.0) // TODO: Visually clamp setting to 1 decimal point
-    val placePerInstant = settings.addInteger("Blocks Placed Per Instant", 1).setMin(1).setMax(20) // TODO: Only Visible when Place Rate is 100
-    val breakRate = settings.addDouble("Block Break Rate %", 100.0).setMin(0.0).setMax(100.0)
-    val crystalRate = settings.addDouble("Crystal Rate %", 100.0).setMin(0.0).setMax(100.0)
-    val attackRate = settings.addDouble("Attack Rate %", 100.0).setMin(0.0).setMax(100.0)
+    fun standardDelay(settings: Setting.Map<*>, name: String, default: Double): Setting.Number<Double> = settings
+        .addDouble(name, default)
+        .setMin(0.0)
+        .setMax(100.0)
+        .setPrecision(1)
+        .appendLines(*percentageMeasureArray)
 
-    fun placeRate(rate: Double) = (round(rate * 10)).toInt()
-    fun breakRate(rate: Double) = (round(rate * 10)).toInt()
-    fun crystalRate(rate: Double) = (round(rate * 10)).toInt()
-    fun attackRate(rate: Double) = (round(rate * 10)).toInt()
+    val block_place_delay = standardDelay(settings, "Block Place Delay", 5.0)
+
+    val place_per_instant = settings
+        .addInteger("Blocks Placed Per Instant", 1)
+        .setMin(1)
+        .setMax(20)
+        .setHidden { block_place_delay.value == 0.0 }
+        .appendLines(*percentageMeasureArray)
+
+    val block_break_delay = standardDelay(settings, "Block Break Delay", 5.0)
+    val crystal_place_delay = standardDelay(settings, "Crystal Place Delay", 5.0)
+    val attack_delay = standardDelay(settings, "Attack Delay", 62.5)
+
+    fun delaySettingAsMillis(setting: Setting.Number<Double>) = (setting.value * 10).toInt()
+    fun millisToTick(value: Int) = ceil(value.toDouble() / 50).toInt() // ceil because higher is safer with delays
 }
